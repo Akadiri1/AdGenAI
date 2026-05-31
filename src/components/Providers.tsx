9ab8cf4c4f6086293489735266213c2145cbfed4
Emmanuel Okikiola <akadiriokiki@gmail.com>
2026-05-31 20:54:24 +0100

    Fix ConfirmProvider missing from Providers root

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -2,6 +2,7 @@
 
 import { SessionProvider } from "next-auth/react";
 import { ToastProvider } from "@/components/ui/Toast";
+import { ConfirmProvider } from "@/components/ui/ConfirmModal";
 import { LangProvider } from "@/components/LangProvider";
 import type { LangCode } from "@/lib/i18n";
 
@@ -15,7 +16,9 @@ export function Providers({
   return (
     <SessionProvider>
       <LangProvider initialLang={initialLang}>
-        <ToastProvider>{children}</ToastProvider>
+        <ToastProvider>
+          <ConfirmProvider>{children}</ConfirmProvider>
+        </ToastProvider>
       </LangProvider>
     </SessionProvider>
   );
